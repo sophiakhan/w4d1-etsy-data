fb0ad04c66c74ae1f82a2c4f36c5118f37d1ce2a
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-20 15:54:31 -0500

    Created first 4 functions

---
+++ etsy.js
@@ -0,0 +1,47 @@
+// 1. Average price of all items
+var total = 0;
+
+items.forEach(getPrices);
+
+function getPrices(items) {
+    total += items.price;
+}
+
+console.log('The average price is' + ' ' + '$' + Math.round((total / items.length) * 100) / 100);
+
+// var result = getAveragePrice(items);
+// function getAveragePrice(items) {
+//     var total = items.reduce(function (prev, next) {
+//         return {price: prev.price + next.price};
+//     }, {price: 0});
+//     console.log(total);
+//     var averagePrice = Math.round((total.price / items.length) * 100) / 100;
+//     console.log(averagePrice);
+//     return averagePrice;
+// }
+ 
+//  console.log('The average price is' + ' $' + result);
+
+// 2. An array of items that cost between $14.00 and $18.00 USD
+var priceArray = items.filter(function(item) {
+    return (item.price >= 14.00 && item.price <= 18.00);
+});
+
+console.log(priceArray); 
+
+// 3. Find the item with a "GBP" currency code and print its name and price
+var gbpCode = items.filter(function(item) {
+    return (item.currency_code === "GBP");
+});
+
+console.log(gbpCode);
+
+// 4. Show how to find which items are made of wood
+var searchTerm = 'wood';
+var woodMaterial = items.filter(function(item) {
+    return item.description.includes(searchTerm);
+});
+
+console.log(woodMaterial);
+
+// 5. Find which items are made of eight or more materials
--- objects-arrays.js
+++ objects-arrays.js
@@ -35,4 +35,146 @@ function getSongTitles(listOfSongs) {
 }
 
 console.log(songTitles); 
-da
+
+// var list = new Array(1, 2, 3);
+// var list = [1, 2, 3];
+
+// var object = new Object();
+// var object = {};
+
+// function drink() {
+//     return 'drank';
+// }
+
+// var waterBottle = {
+//     isFull: false,
+//     oz: 16,
+//     drink: drink
+// };
+
+// var Array = {
+//     length: 15,
+//     pop: function() {}
+// };
+
+
+// console.log(waterBottle.oz)
+// console.log(waterBottle.isFull)
+// console.log(waterBottle.drink())
+
+// fetch('http://swapi.co/api/films/1/')
+// .then(function(res) {
+//     return res.json();
+// })
+// .then(function(data) {
+//     console.log(data);
+// });
+
+// var movies = [
+//     'Pulp Fiction',
+//     'Frozen',
+//     'Ex Machina',
+//     'The Goonies'
+// ];
+
+// console.log(movies);
+// console.log(movies.length);
+
+// console.log(movies[0].toUpperCase());
+
+// movies[1] = 'Wall-E';
+// console.log(movies);
+
+// console.log(movies.pop());
+// console.log(movies);
+
+// var emailAddress = 'collin@theironyard.com';
+// var domainName = emailAddress.split('@').pop();
+// console.log(domainName);
+
+// console.log(movies.slice(1));
+
+
+
+// for (var i = 0; i < movies.length; i++) {
+//     // console.log(i);
+//     var movieTitle = movies[i].title;
+//     // console.log(movieTitle);
+// }
+
+// movies.forEach(function(movie) {
+//     console.log(movie);
+//     console.log(movie.title);
+//     console.log(movie.year);
+// });
+
+// movies.forEach(logMovie);
+
+// function logMovie(movie) {
+//     console.log(movie);
+//     console.log(movie.title);
+//     console.log(movie.year);
+// }
+
+
+// var total = 0;
+
+// prices.forEach(showPrices);
+
+// function showPrices(price) {
+//     total += price;
+// }
+
+// console.log(total);
+
+// var prices = [12.54, 12.68, 12.78, 12.00];
+
+// var total = getTotal(prices);
+
+// function getTotal(prices) {
+//     return prices.reduce(function(first, second) {
+//         return first + second;
+//     }, 0);
+// }
+
+// console.log(total);
+
+
+
+// var movieTitles = [];
+
+// movies.forEach(function(movie) {
+//     movieTitles.push(movie.title);
+// });
+
+// console.log(movieTitles);
+
+var movies = [
+    {title: 'Pulp Fiction', year: 1996},
+    {title: 'Frozen', year: 2014},
+    {title: 'Ex Machina', year: 2015},
+    {title: 'The Goonies', year: 1986}
+];
+
+var movieYears = getMovieYears(movies);
+
+function getMovieYears(listOfMovies) {
+    return listOfMovies.map(function(movie) {
+        return {
+            movieTitle: movie.title.toUpperCase(),
+            movieYear: movie.year
+        };
+    });
+}
+
+console.log(movieYears);
+
+// var names = ['Bart', 'Hemor', 'Morty'];
+// var searchTerm = 'o';
+
+var matches = names.filter(function(name) {
+    return name.toLowerCase().includes(searchTerm.toLowerCase());
+    // return name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+});
+
+console.log(matches);
