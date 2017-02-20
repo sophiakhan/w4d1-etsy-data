9942ce5215b78da31d9acc897bf193caf85e3c57
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-20 16:38:22 -0500

    Corrected the 6th function

--- etsy.js
+++ etsy.js
@@ -9,18 +9,19 @@ function getPrices(items) {
 
 console.log('The average price is' + ' ' + '$' + Math.round((total / items.length) * 100) / 100);
 
-// var result = getAveragePrice(items);
-// function getAveragePrice(items) {
-//     var total = items.reduce(function (prev, next) {
-//         return {price: prev.price + next.price};
-//     }, {price: 0});
-//     console.log(total);
-//     var averagePrice = Math.round((total.price / items.length) * 100) / 100;
-//     console.log(averagePrice);
-//     return averagePrice;
-// }
- 
-//  console.log('The average price is' + ' $' + result);
+    // var result = getAveragePrice(items);
+    // function getAveragePrice(items) {
+    //     var total = items.reduce(function (prev, next) {
+    //         return {price: prev.price + next.price};
+    //     }, {price: 0});
+    //     console.log(total);
+    //     var averagePrice = Math.round((total.price / items.length) * 100) / 100;
+    //     console.log(averagePrice);
+    //     return averagePrice;
+    // }
+    
+    //  console.log('The average price is' + ' $' + result);
+
 
 // 2. An array of items that cost between $14.00 and $18.00 USD
 var priceArray = items.filter(function(item) {
@@ -29,12 +30,14 @@ var priceArray = items.filter(function(item) {
 
 console.log(priceArray); 
 
+
 // 3. Find the item with a "GBP" currency code and print its name and price
 var gbpCode = items.filter(function(item) {
     return (item.currency_code === "GBP");
 });
 
 console.log(gbpCode);
+
 
 // 4. Show how to find which items are made of wood
 var searchTerm = 'wood';
@@ -44,6 +47,7 @@ var woodMaterial = items.filter(function(item) {
 
 console.log(woodMaterial);
 
+
 // 5. Find which items are made of eight or more materials
 var eightOrMoreMaterials = items.filter(function(item) {
     return(item.materials.length >= [8]);
@@ -51,9 +55,11 @@ var eightOrMoreMaterials = items.filter(function(item) {
 
 console.log(eightOrMoreMaterials);
 
+
 // 6. calculate how many items were made by their sellers
+var searchWho = 'i_did'
 var totalSold = items.filter(function(item) {
-    return item.listing_id;
+    return item.who_made.includes(searchWho);
 }).length;
 
 console.log(totalSold + ' ' + 'were made by their sellers');
